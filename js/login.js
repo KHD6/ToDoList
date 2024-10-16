daa9cf5050072d7228dfd6682703cef494522755
김홍덕 <akacord6@gmail.com>
2024-10-17 07:37:57 +0900

    스크립트 기능 완료

--- js/login.js
+++ js/login.js
@@ -12,18 +12,28 @@ function onLoginSubmit(event) {
     // step2. form을 다시 숨겨준다
     const username = loginInput.value;
     // step3. value를 username이라는 key값으로 저장시켜준다
-    localStorage.setItem(USERNAME_KEY, username);
+    localStorage.setItem(USERNAME_KEY, JSON.stringify(username));
     // step4. username값을 username이라는 key와 함께 local storeage에 저장한다
     paintGreetings(username);
     // onLoginSubmit함수에서는 유저정보가 input으로부터 오고 있다
 }
 
+function deletGreeting() {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, "");
+    greeting.querySelector("button").remove();
+}
+
 function paintGreetings(username) {
-    greeting.querySelector("p").innerText = `Hello ${username}`;
+    const userNameGet = localStorage.getItem(USERNAME_KEY)
+    const userNameText = `Hello ${userNameGet}`
+    greeting.querySelector("p").innerText = userNameText;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     const button = document.createElement("button");
-    button.innerText = `저는 ${username}(이)가 아닙니다.`
+    button.innerText = `저는 ${userNameGet}(이)가 아닙니다.`
     greeting.appendChild(button)
+    button.addEventListener("click", deletGreeting)
 }
 // step5. 비어있는 p 요소안에 `Hello username` 이라는 텍스트 추가
 
@@ -32,8 +42,9 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 // step6. 앱이 시작되면 local storage에서 savedusername을 얻으려고 할텐데 거기서 username이라는 key를 가지고 찾게 된다
 
 // step7. 처음에는 key랑 value가 null이므로
-if (savedUsername === null) {
+if (savedUsername === null || savedUsername=== "") {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.add(HIDDEN_CLASSNAME);
     // form에 hidden을 지워주고
     loginForm.addEventListener("submit", onLoginSubmit);
     // form이 submit될때만 onLoginSubmit함수를 실행 시키도록 한다
--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo){
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "X"
+    button.innerText = "닫기"
     button.addEventListener("click", deleteToDo)
     li.appendChild(span);
     li.appendChild(button);
