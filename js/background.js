a7a86e17e079d1fa6ad25f25e035bab553bbe3bb
김홍덕 <akacord6@gmail.com>
2024-10-17 23:22:59 +0900

    배경이미지 수정

--- js/background.js
+++ js/background.js
@@ -2,6 +2,8 @@ const bgimages = [
     "bg-img01.jpg",
     "bg-img02.jpg",
     "bg-img03.jpg",
+    "bg-img04.jpg",
+    "bg-img05.jpg",
 ];
 
 const chosenBgImgage = bgimages[Math.floor(Math.random() * bgimages.length)];
